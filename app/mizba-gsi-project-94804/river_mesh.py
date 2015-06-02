6956f6b3f0e94f68616ce89aa4d3b3eb4051cc34
Kosuke Mizubayashi <miz@afpa.local.mizba.net>
2015-06-02 22:55:52 +0900

    Move cell optimization logic to server-side (Python) from client-side (Javascript).

--- app/mizba-gsi-project-94804/river_mesh.py
+++ app/mizba-gsi-project-94804/river_mesh.py
@@ -3,6 +3,21 @@ import os
 import webapp2
 import json
 
+E = 0.0001
+
+_SOUTH = 1;
+_WEST = 0;
+_NORTH = 3;
+_EAST = 2;
+
+_SW = 0;
+_SE = 1;
+_NE = 2;
+_NW = 3;
+
+def f_equals(f0, f1):
+  return -E < (f0 - f1) and (f0 - f1) < E
+
 class River(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/javascript'
@@ -61,12 +76,33 @@ class RiverMesh(webapp2.RequestHandler):
             )
     cursor = db.cursor()
     cursor.execute(query, {'code':code})
-    points = []
+    rects = []
+    r = None
     for row in cursor.fetchall():
-      points.append(row)
+      p = [row[0], row[1], row[2], row[3]]
+      if not r:
+        r = p
+        continue
+
+      if (f_equals(r[_SOUTH], p[_SOUTH]) and f_equals(r[_NORTH], p[_NORTH]) and
+          f_equals(r[_EAST], p[_WEST])):
+        r[_EAST] = p[_EAST]
+      else:
+        rects.append([[r[_SOUTH], r[_WEST]],
+                      [r[_SOUTH], r[_EAST]],
+                      [r[_NORTH], r[_EAST]],
+                      [r[_NORTH], r[_WEST]],
+                      [r[_SOUTH], r[_WEST]]])
+        r = p
+
+    rects.append([[r[_SOUTH], r[_WEST]],
+                  [r[_SOUTH], r[_EAST]],
+                  [r[_NORTH], r[_EAST]],
+                  [r[_NORTH], r[_WEST]],
+                  [r[_SOUTH], r[_WEST]]])
 
     self.response.write('%(callback)s && %(callback)s(%(json)s);\n'
-                        % {'callback':str(callback), 'json':json.dumps(points)})
+                        % {'callback':str(callback), 'json':json.dumps(rects)})
     db.close()
 
 
