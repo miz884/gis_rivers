98f00bee9cc7984bdc6916e49f4d7f9d1ba74534
Kosuke Mizubayashi <miz@mizba.net>
2015-06-18 00:22:43 +0900

    Add a new table for compact river_mesh data.

--- data/db_schema.sql
+++ data/db_schema.sql
@@ -1,12 +1,26 @@
-create table kanto_mesh (
-  river_code VARCHAR(10),
-  river_name VARCHAR(50),
-  min_lng DOUBLE,
-  min_lat DOUBLE,
-  max_lng DOUBLE,
-  max_lat DOUBLE
-);
+USE kanto_rivers;
 
-create index river_code_i on kanto_mesh(river_code);
+-- create table kanto_mesh (
+--   river_code VARCHAR(10),
+--   river_name VARCHAR(50),
+--   min_lng DOUBLE,
+--   min_lat DOUBLE,
+--   max_lng DOUBLE,
+--   max_lat DOUBLE
+-- );
+-- create index river_code_i on kanto_mesh(river_code);
 
+
+DROP TABLE IF EXISTS compact_kanto_mesh;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+
+CREATE TABLE compact_kanto_mesh (
+  modified_mesh_code BIGINT,
+  river_code         BIGINT,
+KEY idx_mesh_code (modified_mesh_code),
+KEY idx_river_code (river_code)
+) DEFAULT CHARSET=utf8;
+
+SET character_set_client = @saved_cs_client;
 
