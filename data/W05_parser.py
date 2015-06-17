ceb0f60b0fcf057a5e98c0d3c1f409116050f558
Kosuke Mizubayashi <miz@mizba.net>
2015-06-17 21:11:25 +0900

    Parse river path XML files directly.

--- data/W05_parser.py
+++ data/W05_parser.py
@@ -8,39 +8,94 @@ import codecs
 
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
-BASE_DIR = '/tmp/kml/'
+BASE_DIR = '/tmp/W05_river_path/'
+
 
 def main():
-  isInPm = False
-  buffer = ''
-  filename = ''
+  curves = {}
+  streams = {}
+  curr_curve_id = None
+  curr_curve_ref = None
+
   for line in sys.stdin:
-    buffer += line
-    if re.match('.*<Placemark>.*', line):
-      isInPm = True
-      buffer = line
-      continue
+    line = line.strip()
+    '''
+    Sample curve:
+    -----------------------------------
+    <gml:Curve gml:id="c-1">
+        <gml:segments>
+                <gml:LineStringSegment>
+                        <gml:posList>
+    35.88983818 139.01777656
+    ...
+    35.88956978 139.01798399
+                        </gml:posList>
+                </gml:LineStringSegment>
+        </gml:segments>
+    </gml:Curve>
+    -----------------------------------
+    '''
+    match = re.match(r'<gml:Curve gml:id="(c-\d*)">', line)
+    if match:
+      curr_curve_id = match.group(1)
+      if not curr_curve_id in curves:
+        curves[curr_curve_id] = []
 
-    if re.match('.*</Placemark>.*', line):
-      isInPm = False
-      if not os.path.isfile(filename):
-        f = open(filename, 'w')
-        f.write('<?xml version="1.0" encoding="utf-8" ?>\n')
-        f.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
-        f.write('<Document id="root_doc">\n')
-        f.write(buffer)
-        f.close()
-      else:
-        f = open(filename, 'a')
-        f.write(buffer)
-        f.close()
-      continue
+    match = re.match(r'^\d*\.\d* \d*\.\d*$', line)
+    if match and curr_curve_id:
+      curves[curr_curve_id].append(line)
 
-    if isInPm:
-      match = re.match('.*<SimpleData name="W05_002">(\d*)<.*', line)
-      if match:
-        filename = BASE_DIR + match.group(1) + '.kml'
-  
+    match = re.match(r'</gml:Curve>', line)
+    if match:
+      curr_curve_id = None
+
+    '''
+    Sample stream:
+    -----------------------------------
+    <ksj:Stream gml:id="r-1">
+        <ksj:waterSystemCode codeSpace="WaterSystemTypeCode.xml">830305</ksj:waterSystemCode>
+        <ksj:location xlink:href="#c-1"/>
+        <ksj:riverCode codeSpace="RiverTypeCode.xml">8303050000</ksj:riverCode>
+        <ksj:sectionType>0</ksj:sectionType>
+        <ksj:riverName>小川谷</ksj:riverName>
+        <ksj:originalDataType>3</ksj:originalDataType>
+        <ksj:flowDirection>1</ksj:flowDirection>
+        <ksj:startRiverNode xlink:href="#gb03_1300005"/>
+        <ksj:endRiverNode xlink:href="#gb03_1300079"/>
+        <ksj:startStreamNode xlink:href="#gb03_1300005"/>
+        <ksj:endStreamNode xlink:href="#gb03_1300006"/>
+    </ksj:Stream>
+    -----------------------------------
+    '''
+    match = re.match(r'<ksj:location xlink:href="#(c-\d*)"/>', line)
+    if match:
+      curr_curve_ref = match.group(1)
+
+    match = re.match(r'<ksj:riverCode codeSpace="RiverTypeCode.xml">(\d*)</ksj:riverCode>', line)
+    if match and curr_curve_ref:
+      river_code = match.group(1)
+      if not river_code in streams:
+        streams[river_code] = []
+      streams[river_code].append(curves[curr_curve_ref])
+
+    match = re.match(r'</ksj:Stream>', line)
+    if match:
+      curr_curve_ref = None
+
+  for code, paths in streams.iteritems():
+    filename = BASE_DIR + code + '.data'
+    f = None
+    if not os.path.isfile(filename):
+      f = open(filename, 'w')
+    else:
+      f = open(filename, 'a')
+
+    for path in paths:
+      f.write('\n')
+      for point in path:
+        f.write(point + '\n')
+    f.close()
+
 if __name__=="__main__":
   main()
 
