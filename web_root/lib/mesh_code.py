a2d450eda3f6801cf167d84fd830f4e28f96879d
Kosuke Mizubayashi <miz@mizba.net>
2015-06-17 21:55:55 +0900

    Introduce Modified Mesh Code to sort data in natural order.

--- web_root/lib/mesh_code.py
+++ web_root/lib/mesh_code.py
@@ -6,6 +6,41 @@ import logging
 
 log = logging.getLogger('mesh_code')
 
+'''
+Modified Mesh Code:
+
+The standard Mesh Codes are assigned locally.
+The first 4 digits represent a primary area.
+The next 2 digits represent a secondary area in a primary area.
+And the next 2 digits represent an area of next level in a secondary area.
+This is good to divide large data into small sections, but it is not easy
+to order data globally in this format.
+The format of 3rd level area code is: "ppuuqvrw", given that pp, q and r
+represent latitude, uu, v and w represent longitude. In this format,
+even if the code would be sorted in natural orders, the latitude and
+longitude will be back and forth.
+
+I'll introduce Modified Mesh Code in "ppqruuvw" format to sort data globally.
+It can be sorted in natural orders, since the first 4 digits represent
+latitude, then the remaining 4 digits represent longitude. If you iterate
+on the code, you'll go horizontally (west to east) firstly, then count up
+the latitude vertically (south to north).
+
+e.g.
+12345678
+12  5 7  -> 1257
+  34 6 8 -> 3468
+--> 1257 3468
+
+53394547
+53  4 4  -> 5344
+  39 5 7 -> 3957
+--> 5344 3957
+
+'''
+
+eog = logging.getLogger('mesh_code')
+
 d100 = Decimal(100)
 d60 = Decimal(60)
 d45 = Decimal(45)
@@ -16,7 +51,76 @@ d5 = Decimal(5)
 d1 = Decimal(1)
 
 
-def meshcodeToSWNE(code):
+def meshCodeToModifiedMeshCode(code):
+  # from ppuuqvrw to ppqruuvw
+  w = Decimal(code % 10)
+  code /= 10
+  r = Decimal(code % 10)
+  code /= 10
+  v = Decimal(code % 10)
+  code /= 10
+  q = Decimal(code % 10)
+  code /= 10
+  u = Decimal(code % 100)
+  code /= 100
+  p = Decimal(code)
+
+  result = p
+  result *= 10
+  result += q
+  result *= 10
+  result += r
+  result *= 100
+  result += u
+  result *= 10
+  result += v
+  result *= 10
+  result += w
+
+  return int(result)
+
+
+def modifiedMeshCodeToMeshCode(code):
+  # from ppqruuvw to ppuuqvrw
+  w = Decimal(code % 10)
+  code /= 10
+  v = Decimal(code % 10)
+  code /= 10
+  u = Decimal(code % 100)
+  code /= 100
+  r = Decimal(code % 10)
+  code /= 10
+  q = Decimal(code % 10)
+  code /= 10
+  p = Decimal(code)
+
+  result = p
+  result *= 100
+  result += u
+  result *= 10
+  result += q
+  result *= 10
+  result += v
+  result *= 10
+  result += r
+  result *= 10
+  result += w
+
+  return int(result)
+
+
+def modifiedMeshCodeToSWNE(modified_mesh_code):
+  mesh_code = modifiedMeshCodeToMeshCode(modified_mesh_code)
+  return meshCodeToSWNE(mesh_code)
+
+
+def latLngToModifiedMeshCode(lat, lng):
+  mesh_code = latLngToMeshCode(lat, lng)
+  return meshCodeToModifiedMeshCode(mesh_code)
+
+
+def meshCodeToSWNE(mesh_code):
+  code = mesh_code
   w = Decimal(code % 10)
   code /= 10
   r = Decimal(code % 10)
@@ -39,7 +143,7 @@ def meshcodeToSWNE(code):
   return [float(s), float(w), float(n), float(e)]
 
 
-def latLngToMeshcode(lat, lng):
+def latLngToMeshCode(lat, lng):
   # lat
   t = Decimal(lat) * d60
   a = t % d40
@@ -65,11 +169,18 @@ def latLngToMeshcode(lat, lng):
   h = t % d45
   w = t // d45
 
-  return int(
-    1 * w +
-    10 * r +
-    100 * v +
-    1000 * q +
-    10000 * u +
-    1000000 * p)
+  result = p
+  result *= 100
+  result += u
+  result *= 10
+  result += q
+  result *= 10
+  result += v
+  result *= 10
+  result += r
+  result *= 10
+  result += w
+
+  return int(result)
+
 
--- web_root/lib/mesh_code_test.py
+++ web_root/lib/mesh_code_test.py
@@ -6,13 +6,12 @@ from mesh_code import *
 E = 0.000001
 def f_equals(f0, f1):
   return (f0 - f1) < E and (f0 - f1) > -E
-    
+
 
 class TestMesh(unittest.TestCase):
 
-
-  def test_meshcodeToSWNE(self):
-    result = meshcodeToSWNE(53394547)
+  def test_meshCodeToSWNE(self):
+    result = meshCodeToSWNE(53394547)
 
     self.assertEquals(result[0], 35.7000)
     self.assertEquals(result[1], 139.7125)
@@ -24,20 +23,20 @@ class TestMesh(unittest.TestCase):
     self.assertTrue(f_equals(result[3] - result[1], 45.0 / 60.0 / 60.0))
 
     # The NE should be equal to SW of (+1, +1) rect.
-    result0 = meshcodeToSWNE(53394547)
-    result1 = meshcodeToSWNE(53394558)
+    result0 = meshCodeToSWNE(53394547)
+    result1 = meshCodeToSWNE(53394558)
     self.assertEquals(result0[2], result1[0])
     self.assertEquals(result0[3], result1[1])
 
 
-  def test_latLngToMeshcode(self):
-    result = latLngToMeshcode(35.70078, 139.71475)
+  def test_latLngToMeshCode(self):
+    result = latLngToMeshCode(35.70078, 139.71475)
     self.assertEquals(result, 53394547)
 
-    result = latLngToMeshcode(35.69999999999999999999999999, 139.7125)
+    result = latLngToMeshCode(35.69999999999999999999999999, 139.7125)
     self.assertEquals(result, 53394547)
 
-    result = latLngToMeshcode(
+    result = latLngToMeshCode(
       Decimal(357000) / Decimal(10000),
       Decimal(1397125) / Decimal(10000))
     self.assertEquals(result, 53394547)
@@ -46,14 +45,41 @@ class TestMesh(unittest.TestCase):
   def test_refrexive(self):
     code = 53394547
     # code -> latlng -> code
-    result0 = meshcodeToSWNE(code)
-    result1 = latLngToMeshcode(result0[0], result0[1])
+    result0 = meshCodeToSWNE(code)
+    result1 = latLngToMeshCode(result0[0], result0[1])
     self.assertEquals(result1, code)
 
     # code -> latlng -> code -> latlng
-    result2 = meshcodeToSWNE(result1)
+    result2 = meshCodeToSWNE(result1)
     self.assertEquals(result0[0], result2[0])
     self.assertEquals(result0[1], result2[1])
+
+
+  def test_modifiedMeshCodeToMeshCode(self):
+    result = modifiedMeshCodeToMeshCode(12345678)
+    self.assertEquals(result, 12563748)
+
+    result = modifiedMeshCodeToMeshCode(53443957)
+    self.assertEquals(result, 53394547)
+
+
+  def test_meshCodeToModifiedMeshCode(self):
+    result = meshCodeToModifiedMeshCode(12345678)
+    self.assertEquals(result, 12573468)
+
+    result = meshCodeToModifiedMeshCode(53394547)
+    self.assertEquals(result, 53443957)
+
+
+  def test_modifiedMeshCodeToSWNE(self):
+    result = modifiedMeshCodeToSWNE(53443957)
+    self.assertEquals(result[0], 35.7000)
+    self.assertEquals(result[1], 139.7125)
+
+
+  def test_latLngToModifiedMeshCode(self):
+    result = latLngToModifiedMeshCode(35.70078, 139.71475)
+    self.assertEquals(result, 53443957)
 
 
 if __name__ == '__main__':
