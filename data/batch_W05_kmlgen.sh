f9df5d9840694339c835c6cc6f109eec863e597d
Kosuke Mizubayashi <miz@mizba.net>
2015-06-25 22:16:21 +0900

    Generate KMZ files for river_path.

---
+++ data/batch_W05_kmlgen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FROM_PATH=./result/W05_river_path_kml/
+TO_PATH=./result/W05_river_path_kmz/
+
+function kmzgen {
+  echo "processing ${1}..."
+  target=$(basename ${1} | sed -e 's/data/kmz/')
+  cat ${1} | python ./W05_kmlgen.py > /tmp/doc.kml
+  zip -j ${2}/${target} /tmp/doc.kml
+}
+export -f kmzgen
+
+mkdir ${TO_PATH}
+
+find ${FROM_PATH} -name '*.data' -exec bash -c "kmzgen {} ${TO_PATH}" \;
+
+
