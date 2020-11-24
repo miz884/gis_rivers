2243613389d5626bf48530e274aaba52f2126158
Kosuke Mizubayashi <miz@mizba.net>
2020-11-25 08:45:36 +0900

    Replace DB with static data files.

--- web_root/river_mesh.py
+++ web_root/river_mesh.py
@@ -1,8 +1,5 @@
-import MySQLdb
-import os
 import webapp2
 import json
-import polygon
 import mesh_code
 import mesh
 import mesh_indexer
@@ -11,7 +8,7 @@ import pickle
 
 class River(webapp2.RequestHandler):
   def _get_river_name(self, river_code):
-    with open("./W07_river_mesh_index/river_code_index.dump", mode="rb") as f:
+    with open("./data/W07_river_mesh_index/river_code_index.dump", mode="rb") as f:
       index = pickle.load(f)
       if int(river_code) in index:
         return index[int(river_code)]
@@ -30,7 +27,7 @@ class River(webapp2.RequestHandler):
       return
 
 
-    index_facade = mesh_indexer.MeshIndexFacade("./W07_river_mesh_index")
+    index_facade = mesh_indexer.MeshIndexFacade("./data/W07_river_mesh_index")
     modified_mesh_code = mesh_code.latLngToModifiedMeshCode(lat, lng)
     river_code = index_facade.search_by_modified_mesh_code(modified_mesh_code)
 
@@ -53,24 +50,11 @@ class RiverMesh(webapp2.RequestHandler):
       self.response.write('%s && %s(false)\n' % (str(callback), str(callback)))
       return
 
-    if (os.getenv('SERVER_SOFTWARE') and
-      os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
-      db = MySQLdb.connect(unix_socket='/cloudsql/mizba-gsi-project-94804:river-mesh',
-                           user='root', db='river_mesh', charset='utf8')
-    else:
-      db = MySQLdb.connect(host='localhost', user='root')
-
-    query = ('select mesh.modified_mesh_code'
-             ' from river_mesh as mesh'
-             ' where mesh.river_code = %(code)s'
-             ' order by mesh.modified_mesh_code'
-            )
-    cursor = db.cursor()
-    cursor.execute(query, {'code':code})
-
+    data_file = "./data/river_mesh_list/%s/%s" % (code[0:2], code)
     code_list = []
-    for row in cursor:
-      code_list.append(row[0])
+    with open(data_file, mode="r") as f:
+      for line in f:
+        code_list.append(int(line))
 
     result = mesh.MeshMerger.merge(code_list)
     lls = [map(lambda y: mesh_code.modifiedMeshCodeToLatLng(y), x) for x in result]
@@ -78,7 +62,6 @@ class RiverMesh(webapp2.RequestHandler):
     self.response.write('%(callback)s && %(callback)s(%(json)s);\n'
                         % {'callback':str(callback),
                            'json':json.dumps(lls)})
-    db.close()
 
 
 app = webapp2.WSGIApplication([
